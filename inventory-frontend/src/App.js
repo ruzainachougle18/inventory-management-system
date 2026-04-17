43488e5398c6b4cb05821fff01b1d246b3d0f65d
ruzainachougle18 <ruzainachougle18@users.noreply.github.com>
2026-04-17 23:51:59 +0530

    Update App.js

--- inventory-frontend/src/App.js
+++ inventory-frontend/src/App.js
@@ -1,9 +1,11 @@
 import Signup from './pages/Signup';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
       <Signup />
+      <Login />
     </div>
   );
 }
